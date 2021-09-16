041981bd992227926c76da2d394340f712220aa6
p0186797 <76880486+p0186797@users.noreply.github.com>
2021-09-16 13:08:16 -0400

    Update strings1.py

--- strings1.py
+++ strings1.py
@@ -1,4 +1,1 @@
-sentence = input()
-sentence = sentence.lower()
-vowels = sentence.count("a") + sentence.count("e") + sentence.count("i") + sentence.count("o") + sentence.count("u")
-print(vowels)
+
